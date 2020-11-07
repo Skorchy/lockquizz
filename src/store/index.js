833e48936b9ef11d6060f67a2cee84c3141015e0
Skorchy <rose.mickael.web@gmail.com>
2020-11-07 20:09:27 +0100

    fetchRoomInfos on joining a room

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,35 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { db } from "../services/firebaseServices";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    roomInfos: null,
+  },
+
+  mutations: {
+    setRoomInfos(state, payload) {
+      state.roomInfos = payload;
+    },
+  },
+
+  actions: {
+    async fetchRoomInfos(context, payload) {
+      const roomName = payload;
+
+      const roomSnapshot = await db
+        .collection("rooms")
+        .doc(roomName)
+        .get();
+
+      if (roomSnapshot.exists) {
+        const roomInfos = roomSnapshot.data();
+
+        context.commit("setRoomInfos", roomInfos);
+      }
+    },
+  },
+  modules: {},
 });
--- src/views/Room.vue
+++ src/views/Room.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="room-container">
+  <div class="room-container" v-if="$store.state.roomInfos">
     <div class="room-infos"></div>
-    <span style="color:white">Bienvenue dans la room : {{ roomName }}</span>
+    <span style="color:white"
+      >Bienvenue dans la room : {{ $store.state.roomInfos.name }}</span
+    ><br />
+    <span style="color:white">Room ID : {{ $store.state.roomInfos.id }}</span>
+    <br />
+    <span style="color:white"
+      >Room Pw : {{ $store.state.roomInfos.password }}</span
+    >
+    <br />
+    <span style="color:white"
+      >Room Owner : {{ $store.state.roomInfos.owner }}</span
+    >
   </div>
 </template>
 
@@ -12,8 +23,13 @@ export default {
       roomName: this.$route.params.roomName,
     };
   },
+  methods: {
+    fetchRoomInfos() {
+      this.$store.dispatch("fetchRoomInfos", this.roomName);
+    },
+  },
   created() {
-    //fetch room infos
+    this.fetchRoomInfos();
   },
 };
 </script>
