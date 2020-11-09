5f76e55e038e310dc8a40f99950eb82c35a65e51
Skorchy <rose.mickael.web@gmail.com>
2020-11-09 22:51:02 +0100

    fetch content from contentful

---
+++ src/services/contentfulHelper.js
@@ -0,0 +1,8 @@
+let contentful = require("contentful");
+
+let client = contentful.createClient({
+  space: "mhizwr74zla8",
+  accessToken: "zPo4LRwgzH7wVhnTOfO0AgehZSfQx4-8zuBk3qROFbk",
+});
+
+export { client };
--- src/views/Room.vue
+++ src/views/Room.vue
@@ -13,25 +13,80 @@
     <span style="color:white"
       >Room Owner : {{ $store.state.roomInfos.owner }}</span
     >
+
+    <div v-for="question in quizQuestions" :key="question.question">
+      <img :src="question.imgUrl" v-if="question.imgUrl" />
+      <audio
+        autoplay
+        controls
+        :src="question.audioUrl"
+        v-if="question.audioUrl"
+      />
+      <p
+        class="question"
+        :class="[
+          {
+            easy: question.difficulty === 'easy',
+            medium: question.difficulty === 'medium',
+            hard: question.difficulty === 'hard',
+          },
+        ]"
+        style="color:white"
+      >
+        {{ question.question }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
+import { client } from "../services/contentfulHelper";
+import get from "lodash/get";
+
 export default {
   data() {
     return {
       roomName: this.$route.params.roomName,
+      quizQuestions: [],
     };
   },
   methods: {
     fetchRoomInfos() {
       this.$store.dispatch("fetchRoomInfos", this.roomName);
     },
+    async fetchQuizData() {
+      const entries = await client.getEntries();
+      console.log("entries items =>", entries.items);
+
+      this.quizQuestions = entries.items.map((question) => {
+        return {
+          question: question.fields.title,
+          difficulty: question.fields.difficulty,
+          imgUrl: get(question, "fields.image.fields.file.url"),
+          audioUrl: get(question, "fields.audio.fields.file.url"),
+        };
+      });
+    },
   },
+
   created() {
     this.fetchRoomInfos();
+    this.fetchQuizData();
   },
 };
 </script>
 
-<style></style>
+<style>
+.question {
+  border: 1px dashed white;
+}
+.easy {
+  border-color: green;
+}
+.medium {
+  border-color: orange;
+}
+.hard {
+  border-color: red;
+}
+</style>
